250c82d3f925abeaae0104fd95cc5a8ecc133b41
Artur Jaakman <jaakmanartur@gmail.com>
2019-05-23 14:35:31 +0300

    Added NAudio Project.

---
+++ NAudioTest/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NAudioTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private NAudio.Wave.WaveFileReader wave = null;
+        private NAudio.Wave.DirectSoundOut output = null;
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Wave File (*.wav)|*.wav;";
+            if (open.ShowDialog() != true) return;
+
+            DisposeWave();
+
+            wave = new NAudio.Wave.WaveFileReader(open.FileName);
+            output = new NAudio.Wave.DirectSoundOut();
+            output.Init(new NAudio.Wave.WaveChannel32(wave));
+            output.Play();
+
+            PauseButton.IsEnabled = true;
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != null)
+            {
+                if (output.PlaybackState == NAudio.Wave.PlaybackState.Playing) output.Pause();
+                else if (output.PlaybackState == NAudio.Wave.PlaybackState.Paused) output.Play();
+            }
+        }
+
+        private void DisposeWave()
+        {
+            if (output != null)
+            {
+                if (output.PlaybackState == NAudio.Wave.PlaybackState.Playing) output.Stop();
+                output.Dispose();
+                output = null;
+            }
+            if (wave != null)
+            {
+                wave.Dispose();
+                wave = null;
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            DisposeWave();
+        }
+    }
+}
